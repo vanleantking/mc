c3d607a5157c883e44dee2315d0b98cd5ae3d133
levan <vanle@tctav.com>
2018-05-04 14:24:15 +0700

    visit binary operator and update call expression

--- src/main/mc/checker/StaticCheck.scala
+++ src/main/mc/checker/StaticCheck.scala
@@ -5,7 +5,7 @@ package mc.checker
  */
 
 import mc.parser._
-import mc.utils.{Decl, _}
+import mc.utils.{Decl, Id, _}
 import java.io.{File, PrintWriter}
 
 //import mc.codegen.Val
@@ -91,47 +91,218 @@ class TypeChecking(ast: AST) extends BaseVisitor with Utils {
     }
 
     override def visitBlock(ast: Block, c: Any): Any = {
+        val blscope = ast.decl.asInstanceOf[List[Any]]
+        val scope = blscope:::c.asInstanceOf[List[Any]]
         println("scope", c)
-        ast.stmt.map(visit(_,c))
+        ast.stmt.foldLeft(scope)((x,y) => {
+            println("block", y)
+                if(y.isInstanceOf[Expr]){
+                    println("block in check", y)
+                    visit(y,scope)
+                    scope.asInstanceOf[List[Any]]
+                }
+                else{
+                    if(y.isInstanceOf[Block]) {
+                        val cBlock = y.asInstanceOf[Block].decl.asInstanceOf[List[Any]]
+                        val childscope =  cBlock::: scope
+                        visit(y, childscope)
+                        scope.asInstanceOf[List[Any]]
+
+                    } else {
+                      visit(y,scope)
+                    }
+                }.asInstanceOf[List[Any]]
+        })
     }
 
     override def visitBinaryOp(ast: BinaryOp, c: Any): Any = {
-        if (ast.op.toString == "=="){
-            if (ast.left.isInstanceOf[ArrayType] || ast.left.isInstanceOf[ArrayPointerType]) {
-                throw TypeMismatchInExpression(ast)
+        println("enter visit binary", ast)
+        val lt = visit(ast.left, c).asInstanceOf[Type]
+        val rt = visit(ast.right, c).asInstanceOf[Type]
+        println(ast.left, ast.right, lt, rt, ast.left)
+        if (ast.op == "+" || ast.op == "-" || ast.op == "*" || ast.op == "/") {
+            println("type", lt, rt, ast, ast.left)
+            if (lt == IntType) {
+                if (rt == IntType) IntType
+                else if (rt == FloatType) FloatType
+                else throw TypeMismatchInExpression(ast)
             }
+            else if (lt == FloatType) {
+                if (rt == IntType || rt == FloatType) FloatType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else throw TypeMismatchInExpression(ast)
         }
-        val lhs = visit(ast.left,c)
-        val rhs = visit(ast.right, c)
+        else if (ast.op == "<" || ast.op == "<=" || ast.op == ">" || ast.op == ">=") {
+            if (lt == BoolType) throw TypeMismatchInExpression(ast)
+            else if (rt == BoolType) throw TypeMismatchInExpression(ast)
+            else BoolType
+        }
+        else if (ast.op == "==" || ast.op == "!=") {
+            if (lt == BoolType) {
+                if (rt == BoolType) BoolType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else if (lt == IntType)
+            {
+                if (rt == IntType) BoolType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else throw TypeMismatchInExpression(ast)
+        }
+        else if (ast.op == "&&" || ast.op == "||") {
+            if (lt == BoolType && rt == BoolType) BoolType
+            else if (lt != BoolType) throw TypeMismatchInExpression(ast)
+            else throw TypeMismatchInExpression(ast)
+        }
+        else {	//(ast.op == "=")
+            if (lt == IntType) {
+                if (rt == IntType) IntType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else if (lt == FloatType) {
+                if (rt == IntType || rt == FloatType) FloatType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else if (lt == BoolType) {
+                if (rt == BoolType) BoolType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else if (lt == StringType) {
+                if (rt == StringType) StringType
+                else throw TypeMismatchInExpression(ast)
+            }
+            else if (lt.isInstanceOf[ArrayPointerType]) {
+                if (rt.isInstanceOf[ArrayPointerType]) {
+                    if (lt.asInstanceOf[ArrayPointerType].eleType == rt.asInstanceOf[ArrayPointerType].eleType)
+                        lt.asInstanceOf[ArrayPointerType]
+                    else throw TypeMismatchInExpression(ast)
+                }
+                else throw TypeMismatchInExpression(ast)
+            }
+            else throw TypeMismatchInExpression(ast)
+        }
+    }
+
+    override def visitUnaryOp(ast: UnaryOp, c: Any): Any = {
+        val tmp = visit(ast.body, c).asInstanceOf[Type]
+        if (ast.op == "!") {
+            if (tmp == BoolType) BoolType
+            else throw TypeMismatchInExpression(ast)
+        }
+        else {
+            if (tmp == BoolType) throw TypeMismatchInExpression(ast)
+            else tmp
+        }
     }
 
     override def visitId(ast: Id, c: Any): Any = {
         val vars = c.asInstanceOf[List[Decl]].filter(p=>p.isInstanceOf[VarDecl])
         // check undeclare for identifiers
-        if(!vars.asInstanceOf[List[Decl]].exists(x => x.asInstanceOf[VarDecl].variable.name.toString == ast.name.toString))
+        val id = lookupId(ast.name.toString, vars.asInstanceOf[List[VarDecl]])
+        println("visit id", id)
+        if (id == null)
             throw Undeclared(Identifier,ast.name.toString)
+        id.varType
     }
 
     override def visitCallExpr(ast: CallExpr, c: Any): Any = {
         val funcs = c.asInstanceOf[List[Decl]].filter(p=>p.isInstanceOf[FuncDecl])
         // check undeclare for function call
-        if(!funcs.asInstanceOf[List[Decl]].exists(x => x.asInstanceOf[FuncDecl].name.name == ast.method.name.toString))
+        if(!funcs.asInstanceOf[List[FuncDecl]].exists(x => x.name.name == ast.method.name.toString))
             throw Undeclared(Function,ast.method.name.toString)
-
-
-        // TODO Need to check mismatch in size of params not match,...
-
 
         // check undeclare for parameters
         ast.params.map(visit(_,c))
+
+        // check match params number in function call
+        val func = lookupFunc(ast.method.name, funcs.asInstanceOf[List[FuncDecl]])
+        if (func != null && func.param.size != ast.params.size) throw TypeMismatchInExpression(ast)
+
+        // check params match type in function call and function define
+        func.param.zip(ast.params).map{case (x, y) =>
+            val lhs = x.varType
+            val rhs = visit(y,c).asInstanceOf[VarDecl].varType
+            if (lhs == IntType && rhs != IntType) throw TypeMismatchInExpression(ast)
+            else if(lhs == FloatType && (rhs != FloatType || rhs != IntType)) throw TypeMismatchInExpression(ast)
+            else if(lhs == BoolType && rhs != BoolType) throw TypeMismatchInExpression(ast)
+            else if(lhs == StringType && rhs != StringType) throw TypeMismatchInExpression(ast)
+            else {
+              if (rhs.isInstanceOf[ArrayPointerType]) {
+                  if (lhs.asInstanceOf[ArrayPointerType].eleType != rhs.asInstanceOf[ArrayPointerType].eleType)
+                    throw TypeMismatchInExpression(ast)
+              }
+              else throw TypeMismatchInExpression(ast)
+            }
+        }
+        func.returnType
     }
 
     override def visitArrayCell(ast: ArrayCell, c: Any): Any = {
-        val arr = visit(ast.arr, c)
-        val idx = visit(ast.idx, c)
+        val arr = visit(ast.arr, c).asInstanceOf[Type]
+        val idx = visit(ast.idx, c).asInstanceOf[Type]
 
-        if (!arr.isInstanceOf[ArrayType] || !idx.isInstanceOf[IntLiteral]) throw TypeMismatchInExpression(ast)
+//        if (arr != ArrayType || idx != IntType) throw TypeMismatchInExpression(ast)
+        arr.asInstanceOf[ArrayType].eleType
     }
+
+
+//    override def visitIf(ast: If, c: Any): Any = {
+//        val env = c.asInstanceOf[List[Any]]
+//        val tmpEnv = env(0).asInstanceOf[List[List[Decl]]]
+//        if (visit(ast.expr, c).asInstanceOf[Type] != BoolType) throw TypeMismatchInStatement(ast)
+//        if (env(1) == true) c
+//        else {	//(env(1) == false)
+//            if (ast.elseStmt != None) {
+//                val returnPath = List(ast.thenStmt, ast.elseStmt.get).map(x =>
+//                    if (x.isInstanceOf[Block]) x.accept(this, List(List[Decl]() :: tmpEnv, env(1), env(2)))
+//                    else x.accept(this, c)).asInstanceOf[List[Any]]
+//                if (returnPath.filter(_.isInstanceOf[Type] == false).forall(x => x.asInstanceOf[List[Any]](1) == true))
+//                    List(env(0), true, env(2))
+//                else c
+//            }
+//            else {
+//                if (ast.thenStmt.isInstanceOf[Block]) ast.thenStmt.accept(this, List(List[Decl]() :: tmpEnv, env(1), env(2)))
+//                else ast.thenStmt.accept(this, c)
+//                c
+//            }
+//        }
+//    }
+//
+//    override def visitFor(ast: For, c: Any): Any = {
+//        val env = c.asInstanceOf[List[Any]]
+//        val tmpEnv = env(0).asInstanceOf[List[List[Decl]]]
+//        if (ast.expr1.accept(this, c).asInstanceOf[Type] != IntType) throw TypeMismatchInStatement(ast)
+//        if (ast.expr3.accept(this, c).asInstanceOf[Type] != IntType) throw TypeMismatchInStatement(ast)
+//        if (ast.expr2.accept(this, c).asInstanceOf[Type] != BoolType) throw TypeMismatchInStatement(ast)
+//        val newEnv = if (ast.loop.isInstanceOf[Block]) ast.loop.accept(this, List(List[Decl]() :: tmpEnv, env(1), true))
+//        else ast.loop.accept(this, List(env(0), env(1), true))
+//        if (newEnv.isInstanceOf[Type]) List(env(0), env(1), false)
+//        else {
+//          val loopEnv = newEnv.asInstanceOf[List[Any]]
+//          List(loopEnv(0),  loopEnv(1), false)
+//        }
+//    }
+//
+//    override def visitReturn(ast: Return, c: Any): Any = {
+//        val env = c.asInstanceOf[List[Any]]
+//        val expEnv = env(0).asInstanceOf[List[List[Decl]]].flatten.filter(_.isInstanceOf[FuncDecl]).asInstanceOf[List[FuncDecl]]
+//        val funcType = expEnv(0).returnType
+//        val returnType = if (ast.expr == null) null else visit(ast.expr.asInstanceOf[AST], c)
+//        if (funcType == VoidType) {
+//          if (returnType != null) throw TypeMismatchInStatement(ast)
+//        }
+//        else if (funcType == IntType) {
+//          if (returnType != IntType) throw TypeMismatchInStatement(ast)
+//        }
+//        else if (funcType == FloatType) {
+//          if (returnType != IntType && returnType != FloatType) throw TypeMismatchInStatement(ast)
+//        }
+//        else {	//(funcType == BoolType)
+//          if (returnType != BoolType) throw TypeMismatchInStatement(ast)
+//        }
+//        List(env(0), true, env(2))
+//    }
 
     override def visitIntType(ast: IntType.type, c: Any): Any = IntType
 
@@ -144,5 +315,17 @@ class TypeChecking(ast: AST) extends BaseVisitor with Utils {
     override def visitVoidType(ast: VoidType.type, c: Any): Any = VoidType
 
     override def visitArrayType(ast: ArrayType, c: Any): Any = ArrayType
+
+    def lookupFunc(Id:String, lst:List[FuncDecl]):FuncDecl = {
+        val varid = lookup(Id,lst,(s:FuncDecl)=>s.name.name)
+        if(varid != None) varid.get
+        else null
+    }
+
+    def lookupId(Id:String, lst:List[VarDecl]):VarDecl = {
+        val varid = lookup(Id,lst,(s:VarDecl)=>s.variable.name)
+        if(varid != None) varid.get
+        else null
+    }
 
 }
--- src/test/scala/CheckerSuite.scala
+++ src/test/scala/CheckerSuite.scala
@@ -6,107 +6,110 @@ import mc.utils._
   * Created by nhphung on 4/29/17.
   */
 class CheckerSuite extends FunSuite with TestChecker {
-  test("not error 401") {
-    val input = "int a;"
-    val expected = ""
-    assert(checkCkr(input,expected,401))
-  }
+//  test("not error 401") {
+//    val input = "int a;"
+//    val expected = ""
+//    assert(checkCkr(input,expected,401))
+//  }
+//
+//  test("not error 402") {
+//    val input = "int a; int b;"
+//    val expected = ""
+//    assert(checkCkr(input,expected,402))
+//  }
+//
+//  test("not error 403") {
+//    val input = "int a; int b; void main(){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,403))
+//  }
+//
+//  test("not error 404") {
+//    val input = "int a; int b; void main(){} void r(){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,404))
+//  }
+//
+//  test("Redeclare variable 405") {
+//    val input = "int a; int a;"
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,405))
+//  }
+//
+//  test("Redeclare variable 406") {
+//    val input = "int a; void main(){} int a;"
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,406))
+//  }
+//
+//  test("Redeclare variable 407") {
+//    val input = "int a, a;"
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,407))
+//  }
+//
+//  test("Redeclare variable 408") {
+//    val input = "int a, b; void r(){} void r(int a){}"
+//    val expected = "Redeclared Function: r"
+//    assert(checkCkr(input,expected,408))
+//  }
+//
+//  test("Redeclare variable 409") {
+//    val input = "int a, b; void r(){} void v(int a){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,409))
+//  }
+//
+//  test("Redeclare variable 410") {
+//    val input = "int a, b; void r(int a){} void v(int a){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,410))
+//  }
+//
+//  test("Redeclare variable 411") {
+//    val input = "int a, b; void r(int a, int b){} void v(int a){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,411))
+//  }
+//
+//  test("Redeclare variable 412") {
+//    val input = "int a, b; void r(int a, int a){} void v(int a){}"
+//    val expected = "Redeclared Parameter: a"
+//    assert(checkCkr(input,expected,412))
+//  }
+//
+//  test("Redeclare variable 413") {
+//    val input = "int a, b; void r(int a, int b){int n;} void v(int a){}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,413))
+//  }
+//
+//  test("Redeclare variable 414") {
+//    val input = "int a, b; void r(int a, int a){int f;} void v(int a){}"
+//    val expected = "Redeclared Parameter: a"
+//    assert(checkCkr(input,expected,414))
+//  }
 
-  test("not error 402") {
-    val input = "int a; int b;"
-    val expected = ""
-    assert(checkCkr(input,expected,402))
-  }
-
-  test("not error 403") {
-    val input = "int a; int b; void main(){}"
-    val expected = ""
-    assert(checkCkr(input,expected,403))
-  }
-
-  test("not error 404") {
-    val input = "int a; int b; void main(){} void r(){}"
-    val expected = ""
-    assert(checkCkr(input,expected,404))
-  }
-
-  test("Redeclare variable 405") {
-    val input = "int a; int a;"
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,405))
-  }
-
-  test("Redeclare variable 406") {
-    val input = "int a; void main(){} int a;"
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,406))
-  }
-
-  test("Redeclare variable 407") {
-    val input = "int a, a;"
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,407))
-  }
-
-  test("Redeclare variable 408") {
-    val input = "int a, b; void r(){} void r(int a){}"
-    val expected = "Redeclared Function: r"
-    assert(checkCkr(input,expected,408))
-  }
-
-  test("Redeclare variable 409") {
-    val input = "int a, b; void r(){} void v(int a){}"
-    val expected = ""
-    assert(checkCkr(input,expected,409))
-  }
-
-  test("Redeclare variable 410") {
-    val input = "int a, b; void r(int a){} void v(int a){}"
-    val expected = ""
-    assert(checkCkr(input,expected,410))
-  }
-
-  test("Redeclare variable 411") {
-    val input = "int a, b; void r(int a, int b){} void v(int a){}"
-    val expected = ""
-    assert(checkCkr(input,expected,411))
-  }
-
-  test("Redeclare variable 412") {
-    val input = "int a, b; void r(int a, int a){} void v(int a){}"
-    val expected = "Redeclared Parameter: a"
-    assert(checkCkr(input,expected,412))
-  }
-
-  test("Redeclare variable 413") {
-    val input = "int a, b; void r(int a, int b){int n;} void v(int a){}"
-    val expected = ""
-    assert(checkCkr(input,expected,413))
-  }
-
-  test("Redeclare variable 414") {
-    val input = "int a, b; void r(int a, int a){int f;} void v(int a){}"
-    val expected = "Redeclared Parameter: a"
-    assert(checkCkr(input,expected,414))
-  }
-
-  test("Redeclare variable 415") {
-    val input = "int a, b; void r(int a, int b){float c;} int b;"
-    val expected = "Redeclared Variable: b"
-    assert(checkCkr(input,expected,415))
-  }
-
-  test("Not error 416") {
-    val input = "int a, b; void r(int a, int b){float c;} int c; void main(){int a;}"
-    val expected = ""
-    assert(checkCkr(input,expected,416))
-  }
+//  test("Redeclare variable 415") {
+//    val input = "int a, b; void r(int a, int b){float c;} int b;"
+//    val expected = "Redeclared Variable: b"
+//    assert(checkCkr(input,expected,415))
+//  }
+//
+//  test("Not error 416") {
+//    val input = "int a, b; void r(int a, int b){float c;} int c; void main(){int a;}"
+//    val expected = ""
+//    assert(checkCkr(input,expected,416))
+//  }
 
   test("Redeclare variable 417") {
-    val input = "int a, b; void r(int a, int b){float c; c=a+b; c[2] = a/b+b;} int d;"
-    val expected = ""
-    assert(checkCkr(input,expected,417))
+    val input = "int a, b; void r(int a, int b){float c[3]; c=a+b; c[2] = a/b+b;} int d;"
+    val expected = "Type Mismatch In Expression: BinaryOp(=,Id(c),BinaryOp(+,Id(a),Id(b)))"
+    assert(checkCkr(input, expected, 417))
   }
+
+
+
 
   test("Redeclare variable 418") {
     val input = "int a, b; void a(){int c;}"
@@ -122,51 +125,75 @@ class CheckerSuite extends FunSuite with TestChecker {
 
   test("Redeclare variable 420") {
     val input = "void a(int v){int c;} int d, b; "
-    val expected = "Redeclared Variable: a"
+    val expected = ""
     assert(checkCkr(input,expected,420))
   }
 
   test("Redeclare variable 421") {
     val input = "void a(int v){int c;e=a+b;} int d, b; "
-    val expected = "Redeclared Variable: a"
+    val expected = "Undeclared Identifier: e"
     assert(checkCkr(input, expected, 421))
   }
 
 
   test("Redeclare variable 422") {
     val input = "void a(int v){int c;func()=a+b;} int d, b; "
-    val expected = "Redeclared Variable: a"
+    val expected = "Undeclared Function: func"
     assert(checkCkr(input,expected,422))
   }
-
-  test("Redeclare variable 423") {
-    val input = "void a(int v){int c;e[3]=a+b;} int d, b; "
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,423))
-  }
-
+//
+//  test("Redeclare variable 423") {
+//    val input = "void a(int v){int c;e[3]=a+b;} int d, b; "
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,423))
+//  }
+//
   test("Redeclare variable 424") {
     val input = "void a(int v){int c;e(3,c)[3]=a+b;} int d, b; "
-    val expected = "Redeclared Variable: a"
+    val expected = "Undeclared Function: e"
     assert(checkCkr(input, expected, 424))
   }
 
   test("Redeclare variable 425") {
-    val input = "void a(int v){int c;c[3]=d+b;} int d, b; "
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,425))
+    val input = "void a(int v){int c;e(3,c)[3]=a+b;} int d, b; int[] e(){} "
+    val expected = "Type Mismatch In Expression: CallExpr(Id(e),List(IntLiteral(3),Id(c)))"
+    assert(checkCkr(input, expected, 425))
   }
 
   test("Redeclare variable 426") {
-    val input = "void a(int v){int c;v(b)[b]=d+b;} int d, b; int v(){} "
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,426))
+    val input = "void a(int v){int c;e(3,c)[3]=a+b;} int d, b; int[] e(boolean r, int c){} "
+    val expected = "Type Mismatch In Expression: CallExpr(Id(e),List(IntLiteral(3),Id(c)))"
+    assert(checkCkr(input, expected, 426))
   }
 
   test("Redeclare variable 427") {
-    val input = "void a(int v){int c;v(g)[b]=d+b;} int d, b; int v(){} "
-    val expected = "Redeclared Variable: a"
-    assert(checkCkr(input,expected,427))
+    val input = "void a(int v){int c;e(3,c)[3]=a+b;} int d, b; int[] e(string e, int c){} "
+    val expected = "Type Mismatch In Expression: CallExpr(Id(e),List(IntLiteral(3),Id(c)))"
+    assert(checkCkr(input, expected, 427))
+  }
+//
+//  test("Redeclare variable 425") {
+//    val input = "void a(int v){int c;c[3]=d+b;} int d, b; "
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,425))
+//  }
+//
+//  test("Redeclare variable 426") {
+//    val input = "void a(int v){int c;v(b)[b]=d+b;} int d, b; int v(){} "
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,426))
+//  }
+//
+//  test("Redeclare variable 427") {
+//    val input = "void a(int v){int c;v(g)[b]=d+b;} int d, b; int v(){} "
+//    val expected = "Redeclared Variable: a"
+//    assert(checkCkr(input,expected,427))
+//  }
+
+  test("Redeclare variable 428") {
+    val input = "int a, b; void r(int a, int b){float c[3]; c[3]=a+b; c[2] = a/b+b;} int d;"
+    val expected = ""
+    assert(checkCkr(input,expected,428))
   }
 //  test("Type Mismatch In Expression: getInt") {
 //    val input = "void main () {} void main () {}"
